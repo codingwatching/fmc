f24c647d49a1571f391f6981cdd4e878e4e7d965
awowogei <awowogei@fmc.gg>
2023-06-28 14:58:27 +0200

    Add equip animation to held item

--- client/src/player/hand.rs
+++ client/src/player/hand.rs
@@ -1,7 +1,11 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, time::Duration};
 
 use bevy::{
-    math::Vec3A, pbr::NotShadowCaster, prelude::*, render::primitives::Aabb, window::PrimaryWindow,
+    gltf::{Gltf, GltfMesh},
+    math::Vec3A,
+    prelude::*,
+    render::{mesh::VertexAttributeValues, primitives::Aabb},
+    window::PrimaryWindow,
 };
 use fmc_networking::{messages, ConnectionId, NetworkClient, NetworkData};
 
@@ -25,14 +29,18 @@ use super::{
     Player,
 };
 
-pub(super) const ANIMATION_LEN: f32 = 0.25;
-
 pub struct HandPlugin;
 impl Plugin for HandPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(
+        app.insert_resource(SwitchAnimation::default()).add_systems(
             Update,
-            (equip_item, play_use_animation, place_block, send_clicks)
+            (
+                equip_item,
+                play_use_animation,
+                play_switch_animation,
+                place_block,
+                send_clicks,
+            )
                 .run_if(in_state(GameState::Playing)),
         );
     }
@@ -42,26 +50,30 @@ impl Plugin for HandPlugin {
 struct EquippedItem;
 
 #[derive(Component)]
-struct HandAnimationMarker;
+struct HandMarker;
 
 // TODO: Animation can be defined at runtime through config files. Just have some parent file with
 // the animations defined for block items and generic items. And then if you want to make something
 // like a fishing rod, you define your own.
 pub fn hand_setup(commands: &mut Commands) -> Entity {
-    let mut animation_player = AnimationPlayer::default();
-    animation_player.set_elapsed(ANIMATION_LEN);
-
     let entity = commands
         .spawn(SceneBundle::default())
-        .insert(NotShadowCaster)
-        .insert(Name::new("player_hand"))
-        .insert(animation_player)
-        .insert(HandAnimationMarker)
+        .insert(AnimationPlayer::default())
+        .insert(HandMarker)
         .id();
 
     return entity;
 }
 
+#[derive(Resource, Default)]
+struct SwitchAnimation {
+    elapsed: f32,
+    old_transform: Transform,
+    old_offset: f32,
+    new_transform: Transform,
+    new_offset: f32,
+    scene_handle: Handle<Scene>,
+}
 // Equips the item that is selected in any visible interface where equipment=true in the config.
 // There should only ever be one such interface visible, if there are more, it will equip one at
 // random.
@@ -70,6 +82,11 @@ fn equip_item(
     net: Res<NetworkClient>,
     items: Res<Items>,
     models: Res<Models>,
+    gltf_assets: Res<Assets<Gltf>>,
+    gltf_meshes: Res<Assets<GltfMesh>>,
+    meshes: Res<Assets<Mesh>>,
+    animation_clips: Res<Assets<AnimationClip>>,
+    mut switch_animation: ResMut<SwitchAnimation>,
     changed_interface_query: Query<(&Interface, &SelectedItemBox), Changed<SelectedItemBox>>,
     item_box_query: Query<&ItemBox>,
     equipped_entity_query: Query<Entity, With<EquippedItem>>,
@@ -80,10 +97,7 @@ fn equip_item(
             With<EquippedItem>,
         ),
     >,
-    mut animation_query: Query<
-        (&mut AnimationPlayer, &mut Handle<Scene>),
-        With<HandAnimationMarker>,
-    >,
+    hand_scene_query: Query<&Handle<Scene>, With<HandMarker>>,
 ) {
     // equip and unequip when the equipment interface is hidden/shown or the selected box changes
     for (interface, selected) in changed_interface_query.iter() {
@@ -107,47 +121,134 @@ fn equip_item(
 
     // equip new item when the selected item changes.
     for item_stack in changed_equipped_item_query.iter() {
-        let (mut animation_player, mut scene) = animation_query.single_mut();
-        //visibility.is_visible {
+        let scene = hand_scene_query.single();
+
+        switch_animation.old_transform = switch_animation.new_transform;
+        switch_animation.old_offset = switch_animation.new_offset;
+
+        let mut new_transform = Transform::default();
+
         if let Some(item_id) = item_stack.item {
             let item = items.get(&item_id);
             let model = models.get(&item.model_id).unwrap();
+            let gltf = gltf_assets.get(&model.handle).unwrap();
 
-            if &model.handle == scene.as_ref() {
+            // This looks like it is only to prevent triggering the animation when switching
+            // between the same items. The server sends a full interface update anytime an item is
+            // picked up, that is also caught by this.
+            if gltf.scenes[0] == *scene {
                 continue;
             }
 
-            *scene = model.handle.clone();
-            animation_player.start(item.equip_animation.clone());
-            animation_player.set_elapsed(ANIMATION_LEN / 2.0);
+            let gltf_mesh = gltf_meshes.get(&gltf.meshes[0]).unwrap();
+            // Cumbersomely extract aabb height from gltf in an error prone way. I don't know how
+            // to do it through the scenes.
+            let mut min: f32 = 0.0;
+            let mut max: f32 = 0.0;
+            for primitive in gltf_mesh.primitives.iter() {
+                let mesh = meshes.get(&primitive.mesh).unwrap();
+                let Some(VertexAttributeValues::Float32x3(vertices)) = mesh.attribute(Mesh::ATTRIBUTE_POSITION) else {continue};
+                for vertex in vertices.iter() {
+                    min = min.min(vertex[1]);
+                    max = max.max(vertex[1]);
+                }
+            }
+            let height = max - min;
+
+            let animation_handle = gltf.named_animations.get("left_click").unwrap().clone();
+            let animation_clip = animation_clips.get(&animation_handle).unwrap();
+
+            for curve in &animation_clip.curves()[0] {
+                match &curve.keyframes {
+                    Keyframes::Scale(frames) => {
+                        new_transform.scale = *frames.last().unwrap();
+                    }
+                    Keyframes::Translation(frames) => {
+                        new_transform.translation = *frames.last().unwrap();
+                    }
+                    Keyframes::Rotation(frames) => {
+                        new_transform.rotation = *frames.last().unwrap();
+                    }
+                }
+            }
+
+            switch_animation.new_transform = new_transform;
+            switch_animation.new_offset = height;
+            switch_animation.scene_handle = gltf.scenes[0].clone();
+            switch_animation.elapsed = 0.0;
         } else {
-            *scene = Handle::default();
+            switch_animation.scene_handle = Handle::default();
+            switch_animation.elapsed = 0.0;
         }
-        //} else {
-        //    equipped_item.item = None;
-        //    *scene = Handle::default();
-        //}
     }
 }
 
+fn play_switch_animation(
+    time: Res<Time>,
+    mut switch_animation: ResMut<SwitchAnimation>,
+    mut hand_query: Query<(&mut Transform, &mut Handle<Scene>), With<HandMarker>>,
+) {
+    const DURATION: f32 = 0.3;
+
+    let (mut transform, mut scene) = hand_query.single_mut();
+
+    if switch_animation.elapsed < DURATION / 2.0 {
+        let mut new_transform = switch_animation.old_transform;
+        new_transform.translation.y -= switch_animation.elapsed * switch_animation.old_offset;
+        *transform = new_transform;
+
+        if switch_animation.elapsed + time.delta_seconds() > DURATION / 2.0 {
+            *scene = switch_animation.scene_handle.clone();
+        }
+    } else if switch_animation.elapsed < DURATION {
+        let mut new_transform = switch_animation.new_transform;
+        new_transform.translation.y -=
+            (DURATION - switch_animation.elapsed) * switch_animation.new_offset;
+        *transform = new_transform;
+    }
+    switch_animation.elapsed += time.delta_seconds();
+}
+
 fn play_use_animation(
+    items: Res<Items>,
+    models: Res<Models>,
+    gltf_assets: Res<Assets<Gltf>>,
+    animation_clips: Res<Assets<AnimationClip>>,
     window: Query<&Window, With<PrimaryWindow>>,
     mouse_button_input: Res<Input<MouseButton>>,
-    mut equipped_query: Query<&mut AnimationPlayer, With<HandAnimationMarker>>,
+    mut hand_animation_query: Query<&mut AnimationPlayer, With<HandMarker>>,
+    equipped_item_query: Query<&ItemStack, With<EquippedItem>>,
 ) {
-    if mouse_button_input.pressed(MouseButton::Left)
-        || mouse_button_input.just_pressed(MouseButton::Right)
-    {
-        // Only play if not in an interface or settings menu.
-        if window.single().cursor.visible {
-            return;
-        }
+    let Ok(equipped_item) = equipped_item_query.get_single() else {return};
 
-        let mut player = equipped_query.single_mut();
-        if player.elapsed() >= ANIMATION_LEN {
-            player.set_elapsed(0.0);
-            player.resume();
+    // Only play if not in an interface or settings menu.
+    if window.single().cursor.visible {
+        return;
+    }
+
+    let item = if let Some(item_id) = &equipped_item.item {
+        items.get(item_id)
+    } else {
+        return;
+    };
+
+    let model = models.get(&item.model_id).unwrap();
+    let gltf = gltf_assets.get(&model.handle).unwrap();
+    let mut player = hand_animation_query.single_mut();
+
+    if mouse_button_input.pressed(MouseButton::Left) {
+        let animation_handle = gltf.named_animations.get("left_click").unwrap();
+        let animation_clip = animation_clips.get(&animation_handle).unwrap();
+        if mouse_button_input.just_pressed(MouseButton::Left) {
+            player.start_with_transition(animation_handle.clone(), Duration::from_millis(10));
+        } else if player.elapsed() >= animation_clip.duration() {
+            player.start(animation_handle.clone());
         }
+    } else if mouse_button_input.just_pressed(MouseButton::Right) {
+        player.start_with_transition(
+            gltf.named_animations.get("left_click").unwrap().clone(),
+            Duration::from_millis(10),
+        );
     }
 }
 
